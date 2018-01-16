5cea395e7497a240f21411a73e864d89ba99cffc
Dave Briccetti <daveb@davebsoft.com>
2018-01-16 10:42:38 -0800

    Improve code, change to gpiozero

--- ledplay/stickcycle.py
+++ ledplay/stickcycle.py
@@ -1,37 +1,27 @@
-from gpiozero import PWMLED
-import colorsys
 from time import sleep
-import sys
-import RPi.GPIO as GPIO
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_MCP3008
+from colorsys import hsv_to_rgb
+from gpiozero import RGBLED
+from Adafruit_MCP3008 import MCP3008
 
+adc = MCP3008(clk=11, cs=8, miso=9, mosi=10)
+led = RGBLED(14, 20, 21)
 
-# Software SPI configuration:
-CLK  = 11
-MISO =  9
-MOSI = 10
-CS   =  8
-mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
-
-ALL = 14, 20, 21
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(ALL, GPIO.OUT)
-
-pwms = [GPIO.PWM(c, 500) for c in ALL]
-for pwm in pwms:
-    pwm.start(0)
+JOY_X = 0      # Which pin on the A/D converter
+JOY_Y = 1
+MAX_ANALOG = 1023
+MAX_HUE = 950  # Stop at violet (avoid wrapping around to red)
 
 try:
     while True:
-        hue = mcp.read_adc(0)
-        brightness = mcp.read_adc(1)
-        rgb = colorsys.hsv_to_rgb(hue / 1023, 1, brightness / 1023)
-        for i, value in enumerate(rgb):
-            pwms[i].ChangeDutyCycle(value * 100)
-            sleep(.05)
-except:
+        joy_x = adc.read_adc(JOY_X)
+        joy_y = adc.read_adc(JOY_Y)
+        hue = min(joy_x, MAX_HUE)
+        brightness = MAX_ANALOG - joy_y  # Invert so up is brightest
+        hsv = hue / MAX_ANALOG, 1, brightness / MAX_ANALOG  # Hue, saturation, value
+        rgb = hsv_to_rgb(*hsv)  # Red, green, blue
+        led.color = rgb
+        sleep(1/30)
+except KeyboardInterrupt:
     pass
 
-GPIO.cleanup()
-
+led.off()
