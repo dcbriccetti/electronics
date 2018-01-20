0cf81952bf3d36b23a5dcf96f714514867b3095d
Dave Briccetti <daveb@davebsoft.com>
2018-01-19 19:24:17 -0800

    Improve code

--- reaction-web/reaction.py
+++ reaction-web/reaction.py
@@ -10,17 +10,30 @@ from flask import Flask, render_template
 from flask_json import FlaskJSON, as_json
 from gpiozero import Button, LED
 
+# Times below are in seconds
+MIN_WAIT = 1.5
+MAX_WAIT = 5
+WAIT_RANGE = MAX_WAIT - MIN_WAIT
+BLINK_TIME = .1
+WIN_BLINK_TIME = .05
+WIN_BLINKS = 5
+
 class Player:
     def __init__(self, name, button, led):
         self.name = name
         self.button = Button(button)
+        self.button.when_pressed = self.pressed
         self.led = LED(led)
         self.wins = 0
-
-players = (Player('Black', 16, 13),
-           Player('Red',   20, 19),
-           Player('White', 21, 26),
-           Player('Blue',  12,  6))
+        self.fastest_ms = None
+        
+    def pressed(self):
+        pass
+    
+players = (Player('Black',  16, 13),
+           Player('Red',    20, 19),
+           Player('White',  21, 26),
+           Player('Yellow', 12,  6))
 
 app = Flask(__name__)
 json = FlaskJSON(app)
@@ -48,16 +61,21 @@ def reset():
 def status():
     event = queue.get()
     sorted_players = players_by_score()
-    names_scores = [[player.name, player.wins] for i, player in enumerate(sorted_players)]
+    names_scores = [[player.name, player.wins, player.fastest_ms or '']
+                    for i, player in enumerate(sorted_players)]
     return {'event': event, 'scores': names_scores}
 
 
+def pressing_players(): return [player for player in players if player.button.is_pressed]
+
 def find_winners():
+    disqualified = pressing_players()
     start = time()
-    timeout = start + 4
+    sleep(.08)  # Skip any presses that are sooner than human reaction time would allow
+    timeout = start + WAIT_RANGE
 
     while time() < timeout:
-        winners = [player for player in players if player.button.is_pressed]
+        winners = [player for player in pressing_players() if player not in disqualified]
         if winners:
             return winners, time() - start
     return [], 0  # Timed out
@@ -65,16 +83,18 @@ def find_winners():
 
 def game_thread():
     while True:
-        sleep_secs = 2 + random() * 3
-        sleep(sleep_secs)
-        for p in players:
-            p.led.blink(.1, 0, 1)
+        sleep(MIN_WAIT + random() * WAIT_RANGE)
+        for player in players:
+            player.led.blink(BLINK_TIME, 0, 1)
 
         winners, elapsed = find_winners()
         sleep(.4)  # Build suspense about who won
         for player in winners:
-            player.led.blink(.05, .05, 3)
-            queue.put('\n%s wins in %d milliseconds' % (player.name, elapsed * 1000))
+            player.led.blink(WIN_BLINK_TIME, WIN_BLINK_TIME, WIN_BLINKS)
+            elapsed_ms = int(elapsed * 1000)
+            queue.put('\n%s wins in %d milliseconds' % (player.name, elapsed_ms))
+            if not player.fastest_ms or elapsed_ms < player.fastest_ms:
+                player.fastest_ms = elapsed_ms
             player.wins += 1
 
 threading.Thread(target=game_thread).start()
--- reaction-web/static/reaction.js
+++ reaction-web/static/reaction.js
@@ -9,15 +9,15 @@ class ReactionTimeGame {
     }
 
     updateStatus() {
-        console.log('us');
+        console.log('Fetching status');
         $.getJSON('status', {}, (data) => {
-            console.log(data);
+            console.log('Got status');
             $('#event').text(data.event);
             $('#scores tbody').empty();
             const scoresTbody = $('#scores tbody');
             for (let nameScore of data.scores) {
-                console.log(nameScore);
-                scoresTbody.append(`<tr><td>${nameScore[0]}</td><td>${nameScore[1]}</td></tr>`);
+                scoresTbody.append(
+                    `<tr><td>${nameScore[0]}</td><td>${nameScore[1]}</td><td>${nameScore[2]}</td></tr>`);
             }
             setTimeout(game.updateStatus, 100);
         });
